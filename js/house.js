7c1ccfba387bd3cafaf5a647c1dfc13b2660a46a
ceilvt <30483275@qq.com>
2016-09-04 18:15:36 +0800

    海外房产

---
+++ js/house.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wendy on 2016/9/4.
+ */
+/*字段勾选*/
+$('.sec .type1 .ele2').on('click',function(){
+    $(this).toggleClass('bt');
+});
+
+/*人气排名切换*/
+$('.type2 .ele4').on('click',function(){
+    $(this).toggleClass('bt');
+});
