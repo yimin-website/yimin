b6ad418c5c1d30c0bc67d208c9d65cb33f18a146
ceilvt <30483275@qq.com>
2016-09-04 19:27:00 +0800

    百科二级

--- js/haiwai.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    $('#country-flag-more').on('mouseover',function(){
-        $('.country-flag-tips').toggleClass('hide show');
-    });
-    $('#country-flag-more').on('mouseout',function(){
-        $('.country-flag-tips').toggleClass('hide show');
-    });
-})();
---
+++ js/wiki.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wendy on 2016/9/4.
+ */
+/*¥Û÷ﬁ«–ªª*/
+$('.wiki .list-ti .unit span').on('click',function(){
+    $('.wiki .list-ti .unit span').removeClass('bt').eq($(this).index()).addClass('bt');
+});
