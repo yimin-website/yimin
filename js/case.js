5dfbd2b7021bcf3fef0285055c2245f236ecbf2e
ceilvt <30483275@qq.com>
2016-09-04 17:04:14 +0800

    成功案例

---
+++ js/case.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wendy on 2016/9/3.
+ */
+//baner
+var slider = $('.case-img').bxSlider({
+    mode:'horizontal',
+    displaySlideQty:1,//???li?????
+    moveSlideQty: 1,//???li?????
+    auto: true
+});
+
+//------------end baner
