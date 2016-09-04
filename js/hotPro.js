5b4c71fb9d80b4a5f0067bd0c1f3a6d9fc33d973
ceilvt <30483275@qq.com>
2016-09-04 17:05:17 +0800

    热门项目

---
+++ js/hotPro.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wendy on 2016/9/4.
+ */
+$('.sec .type1 .ele2').on('click',function(){
+    $(this).toggleClass('bt');
+});
+/*»À∆¯≈≈√˚«–ªª*/
+$('.type2 .ele4').on('click',function(){
+    $(this).toggleClass('bt');
+});
